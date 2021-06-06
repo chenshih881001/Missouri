0c7cffd0017d063abe21a350ddcb4eb79ba63cac
thlong-phan <72193958+thlong-phan@users.noreply.github.com>
2021-06-06 15:37:37 +0200

    Create Home Page.c

---
+++ Codes/Functions/Home Page.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{           printf("/ \n");
+            printf("      __  __  _                                _ \n");
+            printf("     |  \\/  |(_)                              (_)\n");
+            printf("     | \\  / | _  ___  ___   ___   _   _  _ __  _ \n");
+            printf("     | |\\/| || |/ __|/ __| / _ \\ | | | || '__|| |\n");
+            printf("     | |  | || |\\__ \\\\__ \\| (_) || |_| || |   | |\n");
+            printf("     |_|  |_||_||___/|___/ \\___/  \\__,_||_|   |_|\n");
+            printf("                                                 \n");
+            printf("                                                 \n");
+            printf("/\n");
+            
+            printf("Press any key to Start\n");
+            getch();  
+return 0;
+}
