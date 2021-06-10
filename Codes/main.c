4b48f9f34b0ec04d9bdc34d8b4cb9d40eed22fd3
chenshih881001 <72192211+chenshih881001@users.noreply.github.com>
2021-06-11 01:56:15 +0200

    Update main.c

--- Codes/main.c
+++ Codes/main.c
@@ -1,102 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define HomePage 1
-#define ModeSelection 2
-#define CDL 3             //Choose Difficulty Level
-#define Deployment 4
-#define Battle 5
-#define Result 6
-
-#define SinglePlayer 7
-#define PvP 8
-#define Easy 9
-#define Hard 10
+#include <windows.h>
+#include <conio.h>
 
 int Mode = 0;
 int Level = 0;
+struct Player
+{
+    int grid[10][10];
+    char display[10][10];
+}Player1,Player2;
 
 int main()
 {
-    switch(State)
+
+    Enter_game();           //press [TAB] to start game; loading game
+    Mode = mode_selection();
+    switch(Mode)
     {
-        case HomePage:
-            /*
-                1. Display Msg (Func)
-                2. Press Enter to start (Func)
-            */
-
-        case ModeSelection:
-            /*
-                1. Display Msg (Func)
-                2. Choose Game Mode (Func)
-
-                Mode = SinglePlayer / PvP;
-                if(Mode == SinglePlayer)
-                    State = CDL;
-                else
-                    state = Deployment;
-                break;
-            */
-
-        case CDL:
-            /*
-                1. Display Msg (Func)
-                2. CDL (Func)
-
-                Level = Easy / Hard;
-                state = Deployment;
-                break;
-            */
-
-        case Deployment:
-            /*
-                1. Show grid (Func)
-                2. Deploy ships (Func)
-
-                if(Mode == SinglePlayer)
-                {
-                    if(Level == Easy)
-                    {
-                        FUNCTIONS
-                    }
-                    else
-                    {
-                        FUNCTIONS
-                    }
-                }
-                else
-                {
-                    FUNCTIONS
-                }
-
-                break;
-            */
-
-        case Battle:
-            /*
-                1. Select target (Func)
-                2. Attack target (Func)
-                3. Show attack result (Func)
-                4. Quit (Func)
-                if(win || quit)
-                {
-                    state = Result;
+        case 1:             //PvP
+            deployment(Player1.grid);
+            deployment(Player2.grid);
+            break;
+        case 2:             //Single Player
+            Level = choose_difficulty_level();
+            switch(Level)
+            {
+                case 1:     //Easy
+                    deployment(Player1.grid);
+                    generate_map(Player2.grid);
                     break;
-                }
-            */
-
-        case Result:
-            /*
-                1. Display final result (Func)
-                2. Decide next move (Func)
-                if(Next == HomePage)
-                {
-                    state = HomePage;
-                }
-                else
-                    break;
-            */
+                case 2:     //Hard
+                    deployment_hard(Player1.grid);      //reefs in the map
+                    generate_map(Player2.grid);
+            }
     }
-
 }
